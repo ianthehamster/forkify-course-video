8d55b84b870bb413cd00be606e87cf1a97f8c570
ianthehamster <ianchow9898@gmail.com>
2023-09-10 12:22:35 +0800

    Fixing fractional issue

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -3,6 +3,7 @@ import View from './View';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 // importing fractional library from npm to convert decimals to fraction
+import { Fraction } from 'fractional';
 import fracty from 'fracty';
 
 // View module in recipe (MVC)
@@ -137,7 +138,7 @@ class RecipeView extends View {
                 <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new fracty(ing.quantity).toString() : ''
+                ing.quantity ? new Fraction(ing.quantity).toString() : ''
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
